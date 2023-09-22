0b4b33b1cf350bf69bd40de9890bb4723cb3d2f2
Mamadou Alpha <mamadoualpha618@gmail.com>
2023-09-22 10:20:57 +0000

    good

--- server/middlewares/protect.js
+++ server/middlewares/protect.js
@@ -9,9 +9,9 @@ export const protect = async (req, res, next) => {
             req.user = decoded
             next()
         } catch (error) {
-            res.status(401).send('Not authorized, token failed')
+            res.status(401).json({message: 'Not authorized, token failed'})
         }
     }else{
-        res.status(401).send('Not authorized, no token')
+        res.status(401).json({message: 'Not authorized, no token'})
     }
 }
