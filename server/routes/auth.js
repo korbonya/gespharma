275a29832982ef4efd6dc9eb70d608fc68b7f571
Mamadou Alpha <mamadoualpha618@gmail.com>
2023-09-22 10:22:16 +0000

    good

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,8 @@
 import { createUser, login } from "../controllers/auth.js";
-import { protect } from "../middlewares/protect.js";
 import express from 'express';
 const router = express.Router();
 
-router.post('/signup',protect, createUser);
+router.post('/signup', createUser);
 router.post('/signin', login);
 
 export default router
