efecd7c5b62e0dcb8cf63ffcc9f7d622816d494d
Mamadou Alpha <mamadoualpha618@gmail.com>
2023-09-22 15:53:02 +0000

    good

--- client/src/components/sidebar/index.jsx
+++ client/src/components/sidebar/index.jsx
@@ -1,31 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-export default function index() {
+export default function Sidebar() {
   return (
-    <div>
-        <div className="py-8 px-4">
-            <h1 className="text-3xl text-green-600 font-extrabold">PHarma</h1>
-        </div>
-        <div>
-
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
-
-        </div>
+    <div className="">
+      <div className="py-8 px-4">
+        <h1 className="text-3xl text-green-600 font-extrabold">PHarma</h1>
+      </div>
+      <div>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
     </div>
-  )
+  );
 }
 
 const MenuItem = () => {
-    return (
-        <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 rounded-full bg-green-500"></div>
-            <div className="text-sm font-medium text-gray-700">Dashboard</div>
-        </div>
-    )
-}
-
-
-
+  return (
+    <Link to="/dashboard">
+      <div className="flex items-center  space-x-4 my-4 bg-green-300 py-2 px-2 hover:text-green-50 text-gray-700 hover:bg-green-500 cursor-pointer">
+        <div className="w-6 h-6 rounded-full bg-yellow-300"></div>
+        <h1 className="text-md font-medium ">Dashboard</h1>
+      </div>
+    </Link>
+  );
+};
--- client/src/routes/dashboard.jsx
+++ client/src/routes/dashboard.jsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/sidebar';
 export default function Dashboard() {
   return (
     <div className="flex">
-        <div className="w-56 min-h-screen">
+        <div className="w-56 min-h-screen bg-green-50">
             <Sidebar />
         </div>
         <div>
