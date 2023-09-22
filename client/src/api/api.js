9ad3ff51167495003a7c4599d54dfd1e18410d1a
Mamadou Alpha <mamadoualpha618@gmail.com>
2023-09-22 17:28:57 +0000

    end now

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({ 
-        baseUrl: "http://localhost:5000" ,
+        baseUrl: "https://gespharma.onrender.com",
+        // baseUrl: "http://localhost:5000" ,
         prepareHeaders: (headers) => {
             const user = localStorage.getItem("user");
             const token = JSON.parse(user)?.token;
